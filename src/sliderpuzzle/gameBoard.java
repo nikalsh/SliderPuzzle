12afdb125cc8d12dd8ae86b0aabf145c98dcf13d
nikalsNy <nikalsNy@nikals-Dator>
2018-10-24 21:00:50 +0200

    working keylistener* to mainframe (gameBoard.java) for global keyboard shortcuts such as - 'Q' - will terminate the app
    *workaround with keyEventDispatcher instead of traditional keyListener to avoid having to implement the same keylisteners and methods in every component-class added to the frame

--- src/sliderpuzzle/gameBoard.java
+++ src/sliderpuzzle/gameBoard.java
@@ -1,42 +1,62 @@
 package sliderpuzzle;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 /**
  *
  * @author nikalsh
  */
-public class gameBoard extends JFrame implements KeyListener{
+public class gameBoard extends JFrame {
 
+    class myKeyListener implements KeyEventDispatcher {
+
+        public myKeyListener() {
+        }
+
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent e) {
+            if (e.getID() == KeyEvent.KEY_RELEASED) {
+                if (e.getKeyCode() == KeyEvent.VK_Q) {
+                    System.exit(0);
+                    dispose();
+                } else if (e.getKeyCode() == KeyEvent.VK_R) {
+                        System.out.println("Restarting the game");
+                        //TODO:
+                        //restartGame();q
+                }
+            }
+            return false;
+        }
+
+    }
+
+    private myKeyListener keyListener = new myKeyListener();
     private gamePanel gamePanel;
+    private statsPanel statsPanel;
 
     public gameBoard() {
         gamePanel = new gamePanel(9);
-        this.add(gamePanel);
+        statsPanel = new statsPanel();
+        gamePanel.setBackground(Color.red);
+        statsPanel.setBackground(Color.blue);
+
+        setLayout(new GridLayout(1, 1));
+        add(gamePanel);
+        add(statsPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setAlwaysOnTop(true);
         setLocationRelativeTo(null);
+
         pack();
-        addKeyListener(this);
 
-    }
-    
-    @Override
-    public void keyPressed(KeyEvent e){
-        System.out.println(e.getKeyCode());
-    }
-    
-    @Override
-    public void keyReleased(KeyEvent e){
-        
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println(e);
+        KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        manager.addKeyEventDispatcher(new myKeyListener());
     }
 
 }
