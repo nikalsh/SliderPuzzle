cdb804316b2ed5fd28b874f6ba0c372240f72034
nikalsNy <nikalsNy@nikals-Dator>
2018-10-24 19:19:00 +0200

    SliderPuzzle first commit testing

---
+++ src/sliderpuzzle/gameBoard.java
@@ -0,0 +1,24 @@
+package sliderpuzzle;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author nikalsh
+ */
+public class gameBoard extends JFrame {
+
+    private gamePanel gamePanel;
+
+    public gameBoard() {
+        gamePanel = new gamePanel();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setAlwaysOnTop(true);
+    
+    
+    
+    }
+
+}
