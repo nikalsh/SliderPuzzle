032bf1ea8ef439a934dee25dd5e7f72e865dad7a
johanone <Johan.Johansson@yh.nackademin.se>
2018-10-26 00:03:27 +0200

    dymanic coordinate system up and running, with moving possible, still no function for prohibiting cross field moves

--- src/sliderpuzzle/SliderPuzzle.java
+++ src/sliderpuzzle/SliderPuzzle.java
@@ -3,39 +3,63 @@
  */
 package sliderpuzzle;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class SliderPuzzle {
+import static sliderpuzzle.gameLogic.buttonList;
 
-    public static void main(String[] args) throws MalformedURLException, IOException {
+public class SliderPuzzle implements MouseListener {
 
-        
-        JFrame bottomBitch = new JFrame();
-        
-        gameBoard game = new gameBoard();
-        game.setLayout(new BoxLayout(game.getContentPane(), BoxLayout.Y_AXIS));
-        gamePanel panel = new gamePanel(3);
-        GUIPanel gui = new GUIPanel();
+public static void main (String[] args) throws MalformedURLException, IOException {
+    
+    gameBoard game = new gameBoard ();
+    game.setLayout (new BoxLayout (game.getContentPane (), BoxLayout.Y_AXIS));
+    GUIPanel gui = new GUIPanel ();
+    
+    game.add (new gamePanel (7));
+    statPanel stats = new statPanel ();
+    game.add (stats);
+    game.add (gui);
+    
+    game.pack ();
+    game.setLocationRelativeTo (null);
+    
+    game.setVisible (true);
+    game.setAlwaysOnTop (true);
+    game.setResizable (false);
+    
+    
+}
 
-        statPanel stats = new statPanel();
-        game.add(panel);
-        game.add(stats);
-        game.add(gui);
+@Override
+public void mouseClicked (MouseEvent e) {
 
-        game.pack();
-        game.setLocationRelativeTo(null);
+}
 
-        game.setVisible(true);
-        game.setAlwaysOnTop(true);
-        game.setResizable(false);
-        
-        
-        
+@Override
+public void mousePressed (MouseEvent e) {
+    System.out.println (((JButton) e.getSource ()).getText ());
+    for (int i = 0; i < buttonList.size (); i++) {
+        if (buttonList.get (i).getText ().equals ("")) {
+            buttonList.get (i).setText (((JButton) e.getSource ()).getText ());
+            ((JButton) e.getSource ()).setText ("");
+}}}
 
-      
-    }
+@Override
+public void mouseReleased (MouseEvent e) {
 
+}
+
+@Override
+public void mouseEntered (MouseEvent e) {
+
+}
+
+@Override
+public void mouseExited (MouseEvent e) {
+
+}
 }
