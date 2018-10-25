d5734a5ee5f7efb0ddb22dde77d5876c80c2d132
johanone <Johan.Johansson@yh.nackademin.se>
2018-10-26 00:03:27 +0200

    dymanic coordinate system up and running, with moving possible, still no function for prohibiting cross field moves

--- src/sliderpuzzle/gameLogic.java
+++ src/sliderpuzzle/gameLogic.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface gameLogic {
 
-    List<JButton> buttonList = new ArrayList<>();
-    
+List<sliderButton> buttonList = new ArrayList<> ();
+
+
 }
--- src/sliderpuzzle/gamePanel.java
+++ src/sliderpuzzle/gamePanel.java
@@ -1,12 +1,15 @@
 package sliderpuzzle;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import com.sun.source.doctree.StartElementTree;
+
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 /**
  *
@@ -18,24 +21,36 @@ public class gamePanel extends JPanel implements gameLogic {
 private JLabel label;
 private int gridSize;
 
-public gamePanel (int size) {
+static final Insets insets = new Insets (0, 0, 0, 0);
+
+
+
+public gamePanel (int size)  {
     
-    this.gridSize = size * size;
+    setLayout (new GridBagLayout ());
     
-    for (int i = 1; i < gridSize; i++) {
-        gameLogic.buttonList.add (new sliderButton (Integer.toString (i)));
-        this.add (gameLogic.buttonList.get (i-1));
+    int buttonNumber = 0;
+            int y = 0;
+            
+            while (buttonList.size () < (size * size)) {
+                for (int i = 0, x = 0; i < size; i++, buttonNumber++, x++) {
+                    buttonList.add (new sliderButton ("Nr " + buttonNumber));
+                    addComponent (this, buttonList.get (buttonNumber), x, y, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH);
+                }
+                ++y;
+            }
+buttonList.get (buttonList.size ()-1).setText ("");
+        }
+        
+        private static void addComponent (Container container, Component component, int gridx, int gridy,
+                                          int gridwidth, int gridheight, int anchor, int fill) {
+            GridBagConstraints gbc = new GridBagConstraints (gridx, gridy, gridwidth, gridheight, 1.0, 1.0,
+                    anchor, fill, insets, 0, 0);
+            container.add (component, gbc);
+        }
     }
-    
-    gameLogic.buttonList.add (new sliderButton (""));
-    this.add (gameLogic.buttonList.get(gameLogic.buttonList.size ()-1));
-    
-    setLayout (new GridLayout (size, size));
-    setPreferredSize (new Dimension (500, 500));
-    
-}
-    
-    
 
 
-}
+
+
+    
