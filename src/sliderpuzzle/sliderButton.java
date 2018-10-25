bf22d9afaaf076d35ee89b1096055c929f342987
nikalsNy <nikalsNy@nikals-Dator>
2018-10-25 23:47:25 +0200

    Added TimerTask to keep track of elapsed seconds. JLabel in statPanel updated every second.

--- src/sliderpuzzle/sliderButton.java
+++ src/sliderpuzzle/sliderButton.java
@@ -16,23 +16,60 @@ import javax.swing.event.AncestorListener;
  */
 public class sliderButton extends JButton implements gameLogic {
 
-private ImageIcon icon;
+    private ImageIcon icon;
 
-public sliderButton(String s) {
-    super(s);
-    addActionListener(l -> {
-        System.out.print ("pressed button: ");
-        System.out.println (((sliderButton) l.getSource()).getText  ());
-        for (int i = 0; i < gameLogic.buttonList.size (); i++) {
-            if (gameLogic.buttonList.get (i).getText ().equals ("")) {
-                gameLogic.buttonList.get (i).setText (((sliderButton) l.getSource()).getText ());
-                ((sliderButton) l.getSource ()).setText ("");
+    public sliderButton(String s) {
+        super(s);
+
+        addActionListener(l -> {
+            Object dis = l.getSource();
+            int datPos;
+            JButton temp;
+
+            int disPos = gameLogic.buttonList.lastIndexOf(this) + 1;
+//            System.out.print(
+//                    "pressed button: ");
+//            System.out.println(
+//                    ((sliderButton) dis).getText());
+            for (int i = 0; i < gameLogic.buttonList.size(); i++) {
+                temp = gameLogic.buttonList.get(i);
+                datPos = gameLogic.buttonList.indexOf(temp ) +1;
+                
+                if (temp.getText().equals("")) {
+
+                    if (canThisButtonBeMoved(disPos, datPos)){
+
+                        gameLogic.buttonList.get(i).setText(((sliderButton) dis).getText());
+                        ((sliderButton) dis).setText("");
+                        System.out.println("button was moved");
+                        //TODO increment moves by 1
+
+                    }
+                    
+                }
+
             }
-    
+
         }
-        //TODO:
-        //canButtonBeMovedOrNot();
-    });
-    
-}
+        );
+
+    }
+
+    public boolean canThisButtonBeMoved(int to, int from) {
+//        System.out.println("emptypos: " + from);
+//        System.out.println("dis: " + to);
+        
+        int res = from-to;
+        System.out.println("To: " + to);
+        System.out.println("From: " + from);
+        
+        
+        
+        System.out.println("abs: " +res);
+        
+        System.out.println(Math.abs(res) == 1);
+        
+    return Math.abs(res) == 1;
+    }
+        
 }
--- src/sliderpuzzle/statPanel.java
+++ src/sliderpuzzle/statPanel.java
@@ -5,6 +5,8 @@ package sliderpuzzle;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -14,16 +16,29 @@ import javax.swing.JPanel;
  */
 public class statPanel extends JPanel {
 
-    private JLabel timer = new JLabel("Tid: 00:00");
+    private JLabel timerButton = new JLabel("Tid: 00:00");
     private JLabel moves = new JLabel("Drag: 0");
+    private Timer timer;
+    private int seconds = 0;
 
     public statPanel() {
         setLayout(new GridLayout(2, 0));
         setBackground(Color.yellow);
 
-        add(timer);
+        add(timerButton);
         add(moves);
-
+        timer = new Timer();
+        timer.scheduleAtFixedRate(task, 0, 1000);
     }
+
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+
+            timerButton.setText("Tid: " + seconds);
+            seconds++;
+        }
+
+    };
 
 }
