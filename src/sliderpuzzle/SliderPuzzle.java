5d026aeda0b2500c1ced29f83853c218fb845b19
johanone <Johan.Johansson@yh.nackademin.se>
2018-10-31 00:12:26 +0100

    extending classes mostly cut, opting instead to instantiate components via methods change size function is working but not fully implemented

--- src/sliderpuzzle/SliderPuzzle.java
+++ src/sliderpuzzle/SliderPuzzle.java
@@ -3,6 +3,7 @@
  */
 package sliderpuzzle;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -14,46 +15,112 @@ import javax.swing.*;
 import static sliderpuzzle.gameLogic.buttonList;
 
 public class SliderPuzzle implements MouseListener {
+JFrame game;
+gamePanel gamePanel;
+private JPanel statRow = new JPanel();
+
+private JButton newGame = new JButton("Nytt spel");
+private JButton quit = new JButton("Avsluta");
+private JButton move = new JButton("Flytta");
+private JButton gridSize = new JButton("Grid size");
+
 
 public SliderPuzzle (int size) {
     
     buttonList.clear ();
-    gameBoard game = new gameBoard ();
-    game.setLayout (new BoxLayout (game.getContentPane (), BoxLayout.Y_AXIS));
-    GUIPanel gui = new GUIPanel ();
+    setGame (new JFrame ());
     
-    game.add (new gamePanel (size));
+    game.setLayout (new BoxLayout (game.getContentPane (), BoxLayout.Y_AXIS));
+    
+    setGamePanel (new gamePanel (size));
+    game.add(getGamePanel ());
     statPanel stats = new statPanel ();
     game.add (stats);
-    game.add (gui);
-    
+    game.add (GUIPanel ());
+
     game.pack ();
     game.setLocationRelativeTo (null);
     
     game.setVisible (true);
     game.setAlwaysOnTop (true);
     game.setResizable (true);
+}
+
+public JFrame getGame () {
+    return game;
+}
+
+public SliderPuzzle setGame (JFrame game) {
+    this.game = game;
+    return this;
+}
+
+public void redrawPuzzle (int size) {
+    buttonList.clear ();
+    game = getGame ();
+
+getGamePanel ().removeAll ();
+    game.add (new gamePanel (9));
     
+    game.setLayout (new BoxLayout (game.getContentPane (), BoxLayout.Y_AXIS));
+    game.setLocationRelativeTo (null);
+    
+    game.setVisible (true);
+    game.setAlwaysOnTop (true);
+    game.setResizable (true);
+    game.pack ();
+}
+
+public JPanel getGamePanel () {
+    return gamePanel;
+}
+
+public SliderPuzzle setGamePanel (gamePanel gamePanel) {
+    this.gamePanel = gamePanel;
+    return this;
+}
+
+public SliderPuzzle () {
+
+}
+JPanel GUIPanel() {
+    JPanel GUIPanel = new JPanel ();
+    GUIPanel.setLayout(new FlowLayout (FlowLayout.LEFT, 5, 5));
+    
+    GUIPanel.add(newGame);
+    GUIPanel.add(move);
+    
+    GUIPanel.add(quit);
+    GUIPanel.add(gridSize);
+    
+    newGame.addActionListener(l -> {
+        
+        System.out.println("new game");
+    });
+    
+    quit.addActionListener(l -> {
+        
+        System.out.println("bye");
+        System.exit(0);
+    });
+    
+    move.addActionListener(l -> {
+        System.out.println("make a move");
+        //TODO implement move logic
+    });
+    
+    gridSize.addActionListener(l -> {
+        int size = 8;
+        System.out.println("chooses grid size");
+        redrawPuzzle (8);
+    });
+    return GUIPanel;
 }
 
 public static void main (String[] args) throws MalformedURLException, IOException {
     
-    gameBoard game = new gameBoard ();
-    game.setLayout (new BoxLayout (game.getContentPane (), BoxLayout.Y_AXIS));
-    GUIPanel gui = new GUIPanel ();
-    
-    game.add (new gamePanel (5));
-    statPanel stats = new statPanel ();
-    game.add (stats);
-    game.add (gui);
-    
-    game.pack ();
-    game.setLocationRelativeTo (null);
-    
-    game.setVisible (true);
-    game.setAlwaysOnTop (true);
-    game.setResizable (true);
-    
+SliderPuzzle sliderPuzzle = new SliderPuzzle (5);
+
     
 }
 
