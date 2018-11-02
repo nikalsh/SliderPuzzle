9a7b68342dd98ddbfce492c2fcfca17598b769bf
johanone <Johan.Johansson@yh.nackademin.se>
2018-11-02 13:40:46 +0100

    size changer working

--- src/sliderpuzzle/GUIPanel.java
+++ src/sliderpuzzle/GUIPanel.java
@@ -4,20 +4,32 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  *
  * @author nikalsh
  */
-public class GUIPanel extends JPanel {
-
+public class GUIPanel extends JPanel  {
+static int size;
     private List<RestartGameListener> listeners = new ArrayList<>();
+int selectedIndex;
+String[] gameSizeSettings = {
+        "2	X 2",
+        "3	X 3",
+        "4	X 4",
+        "5	X 5",
+        "6	X 6",
+        "7	X 7",
+        "8	X 8",
+        "9	X 9",
+        "10	X 10",
+        "11	X 11",
+        "12	X 12"
+};
+JComboBox<String> listOfGameSizes = new JComboBox<> (gameSizeSettings);
 
-    
-    
-    private JPanel statRow = new JPanel();
+private JPanel statRow = new JPanel();
 
     private JButton newGame = new JButton("Nytt spel");
     private JButton quit = new JButton("Avsluta");
@@ -26,19 +38,22 @@ public class GUIPanel extends JPanel {
     public void addRGListeners(RestartGameListener toAdd) {
         listeners.add(toAdd);
     }
-
-    GUIPanel() {
+    
+GUIPanel() {
 
         setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
         add(newGame);
         add(move);
-
+        add (listOfGameSizes );
         add(quit);
 
         newGame.addActionListener(l -> {
 
-            System.out.println("new game");
+            
+           GUIPanel.size =  Integer.valueOf (listOfGameSizes.getSelectedIndex ()+2);
+           
+           System.out.println("new game");
             listeners.forEach(RestartGameListener::newGame);
 
         });
@@ -60,5 +75,6 @@ public class GUIPanel extends JPanel {
 //        setBackground(Color.red);
 
     }
-
+    
+    
 }
--- src/sliderpuzzle/GamePanel.java
+++ src/sliderpuzzle/GamePanel.java
@@ -15,277 +15,279 @@ import javax.swing.*;
  */
 public class GamePanel extends JPanel implements RestartGameListener, KBControllerListener {
 
-    int emptyX, emptyY, newX, newY;
+int emptyX, emptyY, newX, newY;
 
-    private List<PaneLListener> paneLListeners = new ArrayList<>();
-    private List<GameStateListener> gameStateListeners = new ArrayList<>();
+private List<PaneLListener> paneLListeners = new ArrayList<> ();
+private List<GameStateListener> gameStateListeners = new ArrayList<> ();
 
-    private ImageHandler imgWiz = new ImageHandler();
-    private JLabel label;
-    private int gridSize;
-    private int size;
-    private BufferedImage image = null;
-    private BufferedImage[][] buttonImage;
-    private SliderButton[][] btnList;
+private ImageHandler imgWiz = new ImageHandler ();
+private JLabel label;
+private int gridSize;
+private int size;
+private BufferedImage image = null;
+private BufferedImage[][] buttonImage;
+private SliderButton[][] btnList;
 
-    String humanDog = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfkVopwJoEyFhWLXYzgNIiV36-Ry12m6KUyX_5gGRk0ACAIOngTw";
-    String monkey = ("https://cdn.vox-cdn.com/thumbor/Or0rhkc1ciDqjrKv73IEXGHtna0=/0x0:666x444/1200x800/filters:focal(273x193:379x299)/cdn.vox-cdn.com/uploads/chorus_image/image/59384673/Macaca_nigra_self-portrait__rotated_and_cropped_.0.jpg");
-    String randomImg = ("https://picsum.photos/500/500/?random");
+String humanDog = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfkVopwJoEyFhWLXYzgNIiV36-Ry12m6KUyX_5gGRk0ACAIOngTw";
+String monkey = ("https://cdn.vox-cdn.com/thumbor/Or0rhkc1ciDqjrKv73IEXGHtna0=/0x0:666x444/1200x800/filters:focal(273x193:379x299)/cdn.vox-cdn.com/uploads/chorus_image/image/59384673/Macaca_nigra_self-portrait__rotated_and_cropped_.0.jpg");
+String randomImg = ("https://picsum.photos/500/500/?random");
 
-    public int getGridSize(){
-        return size;
-    }
+public int getGridSize () {
+    return size;
+}
+
+public GamePanel (int size) throws MalformedURLException {
     
-    public GamePanel (int size) throws MalformedURLException {
+    this.size = size;
+    runPlayState ();
+    randomize ();
+    setBounds (0, 0, 500, 500);
+    setPreferredSize (new Dimension (500, 500));
+    
+}
 
-        this.size = size;
-        runPlayState();
-        randomize();
-        setBounds(0, 0, 500, 500);
-        setPreferredSize(new Dimension(500, 500));
-
-    }
-
-    public void disableButtons() {
-        for (int i = 0; i < btnList.length; i++) {
-            for (int j = 0; j < btnList.length; j++) {
-                btnList[i][j].setEnabled(false);
-            }
+public void disableButtons () {
+    for (int i = 0; i < btnList.length; i++) {
+        for (int j = 0; j < btnList.length; j++) {
+            btnList[i][j].setEnabled (false);
         }
     }
+}
 
-    public void enableButtons() {
-        for (int i = 0; i < btnList.length; i++) {
-            for (int j = 0; j < btnList.length; j++) {
-                btnList[i][j].setEnabled(true);
-            }
+public void enableButtons () {
+    for (int i = 0; i < btnList.length; i++) {
+        for (int j = 0; j < btnList.length; j++) {
+            btnList[i][j].setEnabled (true);
         }
     }
+}
 
-    public void addPaneLListener(PaneLListener toAdd) {
-        paneLListeners.add(toAdd);
+public void addPaneLListener (PaneLListener toAdd) {
+    paneLListeners.add (toAdd);
+}
+
+public void addGameStateListener (GameStateListener toAdd) {
+    gameStateListeners.add (toAdd);
+}
+
+public void runGameIsWonState () {
+    
+    gameStateListeners.forEach (GameStateListener::changeToWinState);
+    disableButtons ();
+    
+}
+
+@Override
+public void newGame () {
+    
+    this.size = GUIPanel.size;
+    
+    this.removeAll ();
+    runPlayState ();
+    
+    randomize ();
+    
+}
+
+public void runPlayState () {
+    gameStateListeners.forEach (GameStateListener::changeToPlayState);
+    
+    try {
+        imgWiz.setURL (monkey);
+    } catch (MalformedURLException ex) {
+        System.out.println ("failed to read URL");
+        
     }
-
-    public void addGameStateListener(GameStateListener toAdd) {
-        gameStateListeners.add(toAdd);
-    }
-
-    public void runGameIsWonState() {
-
-        gameStateListeners.forEach(GameStateListener::changeToWinState);
-        disableButtons();
-
-    }
-
-    @Override
-    public void newGame() {
-
-        this.removeAll();
-        runPlayState();
-
-        randomize();
-
-    }
-
-    public void runPlayState() {
-        gameStateListeners.forEach(GameStateListener::changeToPlayState);
-
-        try {
-            imgWiz.setURL(monkey);
-        } catch (MalformedURLException ex) {
-            System.out.println("failed to read URL");
-
-        }
-        buttonImage = new BufferedImage[size][size];
-
-        image = imgWiz.loadFromUrl();
-
-        image = imgWiz.resize(image, 500.0, 500.0);
-
-        buttonImage = imgWiz.slice(size, size, image);
-
-        btnList = new SliderButton[size][size];
-        ImageIcon ico = null;
-
-        this.gridSize = size * size;
+    buttonImage = new BufferedImage[size][size];
+    
+    image = imgWiz.loadFromUrl ();
+    
+    image = imgWiz.resize (image, 500.0, 500.0);
+    
+    buttonImage = imgWiz.slice (size, size, image);
+    
+    btnList = new SliderButton[size][size];
+    ImageIcon ico = null;
+    
+    this.gridSize = size * size;
 
 //        gameLogic logic = new gameLogic(size);
-        setLayout(new GridLayout(size, size));
-        int k = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                k++;
-
-                SliderButton btn = new SliderButton ((k == this.gridSize ? "" : Integer.toString(k)), j, i, k);
-
-                //slidebuttons
-                if (k != this.gridSize) {
-
-                    ico = new ImageIcon(buttonImage[i][j]);
-                    btn.setFont(new Font("Arial", Font.PLAIN, 0));
-                    btn.setIcon(ico);
-
-                }
-                //the empty button
-                btnList[i][j] = btn;
-                btn.setFont(new Font("Arial", Font.PLAIN, 0));
-                add(btn);
-
-                this.setButtonActionListener(btn);
-
+    setLayout (new GridLayout (size, size));
+    int k = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            k++;
+            
+            SliderButton btn = new SliderButton ((k == this.gridSize ? "" : Integer.toString (k)), j, i, k);
+            
+            //slidebuttons
+            if (k != this.gridSize) {
+                
+                ico = new ImageIcon (buttonImage[i][j]);
+                btn.setFont (new Font ("Arial", Font.PLAIN, 0));
+                btn.setIcon (ico);
+                
             }
-
+            //the empty button
+            btnList[i][j] = btn;
+            btn.setFont (new Font ("Arial", Font.PLAIN, 0));
+            add (btn);
+            
+            this.setButtonActionListener (btn);
+            
         }
-
+        
     }
+    
+}
 
-    public void setButtonActionListener(JButton button) {
-
-        button.addActionListener(l -> {
-            SliderButton A = ((SliderButton) l.getSource());
-            int y = A.y();
-            int x = A.x();
-            int diffY = 0;
-            int diffX = 0;
-            int diff;
-            int b = 0;
-
-            OUTER:
-            for (int yy = 0; yy < btnList.length; yy++) {
-                for (int xx = 0; xx < btnList.length; xx++) {
-
-                    if (btnList[yy][xx].getText().equals("")) {
-
-                        diffY = Math.abs(yy - y);
-                        diffX = Math.abs(xx - x);
-
-                        //TODO: implement several piece movement in swap()
-                        if (diffY == 1 && diffX == 0 || diffX == 1 && diffY == 0) {
-                            swap(A, btnList[yy][xx]);
-                            break OUTER;
-                        }
-
+public void setButtonActionListener (JButton button) {
+    
+    button.addActionListener (l -> {
+        SliderButton A = ((SliderButton) l.getSource ());
+        int y = A.y ();
+        int x = A.x ();
+        int diffY = 0;
+        int diffX = 0;
+        int diff;
+        int b = 0;
+        
+        OUTER:
+        for (int yy = 0; yy < btnList.length; yy++) {
+            for (int xx = 0; xx < btnList.length; xx++) {
+                
+                if (btnList[yy][xx].getText ().equals ("")) {
+                    
+                    diffY = Math.abs (yy - y);
+                    diffX = Math.abs (xx - x);
+                    
+                    //TODO: implement several piece movement in swap()
+                    if (diffY == 1 && diffX == 0 || diffX == 1 && diffY == 0) {
+                        swap (A, btnList[yy][xx]);
+                        break OUTER;
                     }
+                    
                 }
             }
-
-            if (gameIsWon()) {
-
-                System.out.println("game is won");
-                runGameIsWonState();
-            }
+        }
+        
+        if (gameIsWon ()) {
+            
+            System.out.println ("game is won");
+            runGameIsWonState ();
+        }
 
 //                    System.out.println(btnList[btnList.length - 1][btnList.length - 1].pos());
-        });
+    });
+    
+}
 
-    }
-
-    public boolean gameIsWon() {
-        System.out.println("");
-        int k = 0;
-
-        for (int yy = 0; yy < btnList.length; yy++) {
-            for (int xx = 0; xx < btnList[yy].length; xx++) {
-                k++;
-                if (k != this.gridSize) {
-                    System.out.print(btnList[yy][xx].getText().equals(Integer.toString(btnList[yy][xx].pos())) + " ");
-
-                    if (!btnList[yy][xx].getText().equals(Integer.toString(btnList[yy][xx].pos()))) {
-                        return false;
-                    }
+public boolean gameIsWon () {
+    System.out.println ("");
+    int k = 0;
+    
+    for (int yy = 0; yy < btnList.length; yy++) {
+        for (int xx = 0; xx < btnList[yy].length; xx++) {
+            k++;
+            if (k != this.gridSize) {
+                System.out.print (btnList[yy][xx].getText ().equals (Integer.toString (btnList[yy][xx].pos ())) + " ");
+                
+                if (!btnList[yy][xx].getText ().equals (Integer.toString (btnList[yy][xx].pos ()))) {
+                    return false;
                 }
-
             }
-            System.out.println("");
+            
         }
-
-        return true;
+        System.out.println ("");
     }
+    
+    return true;
+}
 
-    public void swap(SliderButton A, SliderButton B) {
-
-        B.setIcon(A.getIcon());
-        A.setIcon(null);
-
-        B.setText(A.getText());
-        A.setText("");
-
-        paneLListeners.forEach(PaneLListener::incrementMove);
-    }
+public void swap (SliderButton A, SliderButton B) {
+    
+    B.setIcon (A.getIcon ());
+    A.setIcon (null);
+    
+    B.setText (A.getText ());
+    A.setText ("");
+    
+    paneLListeners.forEach (PaneLListener::incrementMove);
+}
 
 //    public boolean tileIsMoveable(){
 //        
 //    }
-    public int[] getEmptyTile() {
-        int[] emptyTile = new int[2];
-        for (int yy = 0; yy < btnList.length; yy++) {
-            for (int xx = 0; xx < btnList[yy].length; xx++) {
-                if (btnList[yy][xx].getText().equals("")) {
+public int[] getEmptyTile () {
+    int[] emptyTile = new int[2];
+    for (int yy = 0; yy < btnList.length; yy++) {
+        for (int xx = 0; xx < btnList[yy].length; xx++) {
+            if (btnList[yy][xx].getText ().equals ("")) {
 //                    System.out.println(yy);
 //                    System.out.println(xx);
-
-                    emptyTile[0] = yy;
-                    emptyTile[1] = xx;
-                    break;
-                }
+                
+                emptyTile[0] = yy;
+                emptyTile[1] = xx;
+                break;
             }
         }
-        return emptyTile;
     }
-
-    public void randomize() {
-
-        do {
-
-            int iterations = (gridSize * gridSize * gridSize);
-            String[] dirs = new String[]{"up", "down", "left", "right"};
-            Random rand = new Random();
-
-            for (int i = 0; i < iterations; i++) {
-                move(dirs[rand.nextInt(4)]);
-
-            }
-
-        } while (gameIsWon());
-
-    }
-
-    @Override
-    public void move(String dir) {
-
-        emptyY = getEmptyTile()[0];
-        emptyX = getEmptyTile()[1];
-
-        switch (dir.toLowerCase()) {
-            case "up":
-                newY = getEmptyTile()[0] + 1;
-                newX = getEmptyTile()[1];
-
-                break;
-            case "down":
-                newY = getEmptyTile()[0] - 1;
-                newX = getEmptyTile()[1];
-
-                break;
-            case "left":
-                newY = getEmptyTile()[0];
-                newX = getEmptyTile()[1] + 1;
-
-                break;
-            case "right":
-                newY = getEmptyTile()[0];
-                newX = getEmptyTile()[1] - 1;
-
-                break;
-
-        }
-//        System.out.println("moving");
-
-        try {
-            swap(btnList[newY][newX], btnList[emptyY][emptyX]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("no can do");
-        }
-    }
-
+    return emptyTile;
 }
+
+public void randomize () {
+    
+    do {
+        
+        int iterations = (gridSize * gridSize * gridSize);
+        String[] dirs = new String[]{"up", "down", "left", "right"};
+        Random rand = new Random ();
+        
+        for (int i = 0; i < iterations; i++) {
+            move (dirs[rand.nextInt (4)]);
+            
+        }
+        
+    } while (gameIsWon ());
+    
+}
+
+@Override
+public void move (String dir) {
+    
+    emptyY = getEmptyTile ()[0];
+    emptyX = getEmptyTile ()[1];
+    
+    switch (dir.toLowerCase ()) {
+        case "up":
+            newY = getEmptyTile ()[0] + 1;
+            newX = getEmptyTile ()[1];
+            
+            break;
+        case "down":
+            newY = getEmptyTile ()[0] - 1;
+            newX = getEmptyTile ()[1];
+            
+            break;
+        case "left":
+            newY = getEmptyTile ()[0];
+            newX = getEmptyTile ()[1] + 1;
+            
+            break;
+        case "right":
+            newY = getEmptyTile ()[0];
+            newX = getEmptyTile ()[1] - 1;
+            
+            break;
+        
+    }
+//        System.out.println("moving");
+    
+    try {
+        swap (btnList[newY][newX], btnList[emptyY][emptyX]);
+    } catch (ArrayIndexOutOfBoundsException e) {
+        System.out.println ("no can do");
+    }
+}
+}
+
